5205e0e513adf80fb046406788d6f80a9de4e742
solarized-penguin <solarized.penguin@gmail.com>
2020-12-18 12:54:39 +0100

    - fixing issues with prestart.sh Currently prestart generates correct PYTHONPATH by itself, so alembic works like a charm.

--- docker/prestart.sh
+++ docker/prestart.sh
@@ -1,10 +1,18 @@
 #! /usr/bin/env bash
 
 echo "CURRENT ENVIRONMENT:${app_env}"
-echo "CURRENT PYTHONPATH:${PYTHONPATH}"
 
+PYTHONPATH="${PYTHONPATH}:$(find ./app -name 'database_schema.py' -exec readlink -f {} \;)"
+export PYTHONPATH
+
+printf "CURRENT PYTHONPATH:\n%s", "${PYTHONPATH}"
+
+echo "Waiting 10 seconds to let database start..."
 # Let the DB start
 sleep 10
 
+echo "Running migrations..."
 # Run migrations
 alembic upgrade head
+
+echo "Migrations concluded. All done!"
