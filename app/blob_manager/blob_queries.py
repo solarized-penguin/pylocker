8e72fa9b99d0cd75fa5ddf8db92c530662d4a79d
solarized-penguin <solarized.penguin@gmail.com>
2020-12-22 17:32:54 +0100

    - adding queries for blob manager

---
+++ app/blob_manager/blob_queries.py
@@ -0,0 +1,9 @@
+create_empty_blob = "SELECT lo_creat(-1)"
+
+write_data_to_blob = "SELECT lo_put(CAST(:loid AS OID), :offset, :data)"
+
+read_data_from_blob = "SELECT lo_get(CAST(:loid AS OID), :offset, :length)"
+
+delete_blob = "SELECT lo_unlink(CAST(:loid AS OID))"
+
+get_size_of_blob = ""
--- app/core/database_schema.py
+++ app/core/database_schema.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sa
+from alembic_utils.pg_function import PGFunction
 from sqlalchemy.dialects.postgresql import INTEGER, TEXT, OID
 
 db_schema = sa.MetaData()
@@ -12,3 +13,33 @@ files_table: sa.Table = sa.Table(
     sa.Column('file_path', TEXT, nullable=True, unique=True),
     sa.Column('file_size_bytes', INTEGER, nullable=True)
 )
+
+blob_size_function_query = """
+CREATE OR REPLACE FUNCTION get_lo_size(loid INTEGER)
+RETURNS BIGINT AS $lo_size$
+DECLARE
+    file_descriptor INTEGER;
+    file_size BIGINT;
+BEGIN
+    -- Open large object for reading.
+    -- Parameter "x'40000'" is equivalent to postgres large object mode "INV_READ"
+    -- which is necessary for method to work
+    file_descriptor := lo_open(CAST(loid AS OID), x'40000' :: INT);
+
+    -- Seek to the end
+    -- "Seek" command = "2"
+    PERFORM lo_lseek64(file_descriptor, 0, 2);
+
+    -- Fetch current file position - location of the last byte
+    file_size := lo_tell64(file_descriptor);
+
+    -- Close open file.
+    PERFORM lo_close(file_descriptor);
+
+    RETURN file_size;
+END;
+$lo_size$
+LANGUAGE plpgsql;
+"""
+
+get_blob_size = PGFunction.from_sql(blob_size_function_query)
