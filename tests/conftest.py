ea6f85d78b898841abc93210d532e436bc84def5
solarized-penguin <solarized.penguin@gmail.com>
2020-12-24 14:10:11 +0100

    - schema creation added need to figure out how to add function creation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from typing import Any, AsyncGenerator
+from typing import Any, AsyncGenerator, Generator
 
 import pytest
 from databases import Database
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
 from app.core import Settings, get_settings
+from app.core.database_schema import db_schema
 from .utils import queries
 
 base_db_url: str = 'postgresql://postgres:postgres@0.0.0.0:5433/postgres'
@@ -15,26 +16,22 @@ test_db_name: str = 'pylocker_test_db'
 settings: Settings = get_settings()
 
 
+def create_schema() -> None:
+    engine: Engine = create_engine(settings.postgres_dsn)
+    db_schema.create_all(engine)
+    engine.dispose()
+
+
 @pytest.fixture(scope='session', autouse=True)
-def create_test_database() -> None:
+def create_test_database() -> Generator[None, None, None]:
     engine: Engine = create_engine(base_db_url)
     with engine.connect() as conn:
-        conn.connection.set_isolation_level(
-            ISOLATION_LEVEL_AUTOCOMMIT
-        )
-        conn.execute(
-            queries.drop_database_if_exists \
-                .replace(':dbname', test_db_name)
-        )
-        conn.execute(
-            queries.create_database \
-                .replace(':dbname', test_db_name)
-        )
+        conn.connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        conn.execute(queries.drop_database_if_exists.replace(':dbname', test_db_name))
+        conn.execute(queries.create_database.replace(':dbname', test_db_name))
+        create_schema()
         yield
-        conn.execute(
-            queries.drop_database_if_exists \
-                .replace(':dbname', test_db_name)
-        )
+        conn.execute(queries.drop_database_if_exists.replace(':dbname', test_db_name))
 
 
 @pytest.mark.asyncio
