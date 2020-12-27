6427750fc71a28f4fe23ad7104631173b8b9f02a
solarized-penguin <solarized.penguin@gmail.com>
2020-12-27 14:33:49 +0100

    - upgrading logging mechanism

--- app/security/__init__.py
+++ app/security/__init__.py
@@ -1,5 +1,6 @@
-from fastapi import Depends
+from fastapi import Depends, Request
 from fastapi.security import OAuth2PasswordBearer
+from loguru import logger
 
 from app.security.auth_client import AuthClient
 from app.security.users_schemas import UserInfo
@@ -10,14 +11,21 @@ security_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(
 
 
 def logged_user(
+        request: Request,
         token: str = Depends(security_scheme),
         client: AuthClient = Depends(AuthClient.create_client)
 ) -> UserInfo:
     """
     Returns id of currently logged user.
+    :param request: current request
     :param client: identity provider client
     :param token: access token
     :return: user id
     :rtype: str
     """
-    return client.fetch_user_info(token)
+    user_info = client.fetch_user_info(token)
+
+    logger.info(
+        f'User: {user_info.email} accessed: |{request.method}| => {request.url}'
+    )
+    return user_info
