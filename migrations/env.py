55dd3e8081ee8dc91cf6853330b09446d6322cba
solarized-penguin <solarized.penguin@gmail.com>
2020-12-29 11:38:56 +0100

    - restructuring table

--- migrations/env.py
+++ migrations/env.py
@@ -4,14 +4,14 @@ import alembic
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from app.core.settings import get_settings
+from app.core.settings import Settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = alembic.context.config
 
 # this will override the ini-file sqlalchemy.url path
-config.set_main_option('sqlalchemy.url', get_settings().postgres_dsn)
+config.set_main_option('sqlalchemy.url', Settings.get().postgres_dsn)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
